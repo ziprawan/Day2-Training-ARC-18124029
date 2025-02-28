703d39f9963d7f7b822d32b6c304ec9e82cf1e78
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2025-02-28 11:05:57 +0700

    utils: Change the way elements added to the DOM
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,4 +1,6 @@
+// Constants
 const USD_TO_IDR = 16370;
+const STAR_COLOR = "#ffc400";
 
 /**
  * Convert number into IDR Currency
@@ -30,42 +32,94 @@ function createElement(tagName, attrs, childs = undefined) {
   return elem;
 }
 
+/**
+ * @typedef {Object} ElementTree
+ * @property {keyof HTMLElementTagNameMap} name - The tag name of the element.
+ * @property {Record<string, string>} attrs - The attributes of the element.
+ * @property {(ElementTree|string|number)[]} [childs] - The children elements or text nodes.
+ */
+
+/**
+ * Processes an ElementTree structure.
+ *
+ * @param {(ElementTree|string|number)[]} elements - The root element of the tree.
+ */
+function createTreeElement(elements) {
+  if (!Array.isArray(elements)) {
+    throw new TypeError(`elements parameters is not an Array`);
+  }
+
+  return elements.map((e) =>
+    typeof e === "object"
+      ? createElement(e.name, typeof e.attrs === "object" ? e.attrs : {}, e.childs ? createTreeElement(e.childs) : undefined)
+      : document.createTextNode(e)
+  );
+}
+
 function appendProduct(product) {
   const { thumbnail, title, brand, price, discountPercentage, rating, availabilityStatus } = product;
   const actualPrice = price * USD_TO_IDR;
   const afterDiscount = (actualPrice * (100 - discountPercentage)) / 100;
   const discount = Math.ceil(discountPercentage);
 
-  const container = createElement("div", { class: "product-container" }, [
-    createElement("div", { class: "product-image-container" }, [
-      createElement("img", { class: "thumbnail", src: thumbnail }),
-    ]),
-    createElement("div", { class: "product-summary" }, [
-      createElement("div", { class: "product-face" }, [
-        createElement("p", { class: "title truncate semibold" }, [title]),
-        createElement("p", { class: "brand truncate" }, [brand ?? "Common Items"]),
-      ]),
-      createElement("div", {}, [
-        createElement("p", { class: "product-discount" }, [
-          createElement("span", { class: "discount-label semibold" }, [`-${discount}%`]),
-          createElement("span", { class: "strikethrough" }, [formatCurrency(actualPrice)]),
-        ]),
-        createElement("p", { class: "product-price semibold" }, [formatCurrency(afterDiscount)]),
-      ]),
-      createElement("p", { class: "product-review truncate" }, [
-        createElement("span", { "data-lucide": "star", fill: "#ffc400", stroke: "#ffc400", class: "star icon" }),
-        rating,
-        " - ",
-        availabilityStatus,
-      ]),
-    ]),
+  const created = createTreeElement([
+    {
+      name: "div",
+      attrs: { class: "product-container" },
+      childs: [
+        {
+          name: "div",
+          attrs: { class: "product-image-container" },
+          childs: [{ name: "img", attrs: { class: "thumbnail", src: thumbnail } }],
+        },
+        {
+          name: "div",
+          attrs: { class: "product-summary" },
+          childs: [
+            {
+              name: "div",
+              attrs: { class: "product-face" },
+              childs: [
+                { name: "p", attrs: { class: "title truncate semibold" }, childs: [title] },
+                { name: "p", attrs: { class: "brand truncate" }, childs: [brand ?? "No brand."] },
+              ],
+            },
+            {
+              name: "div",
+              childs: [
+                {
+                  name: "p",
+                  attrs: { class: "product-discount" },
+                  childs: [
+                    { name: "span", attrs: { class: "discount-label semibold" }, childs: [`-${discount}%`] },
+                    { name: "span", attrs: { class: "strikethrough" }, childs: [formatCurrency(actualPrice)] },
+                  ],
+                },
+                { name: "p", attrs: { class: "product-price semibold" }, childs: [formatCurrency(afterDiscount)] },
+              ],
+            },
+            {
+              name: "p",
+              attrs: { class: "product-review truncate" },
+              childs: [
+                { name: "span", attrs: { "data-lucide": "star", fill: STAR_COLOR, stroke: STAR_COLOR, class: "star icon" } },
+                rating,
+                " - ",
+                availabilityStatus,
+              ],
+            },
+          ],
+        },
+      ],
+    },
   ]);
 
-  document.getElementById("product-list")?.append(container);
+  document.getElementById("product-list")?.append(created[0]);
 }
 
 utils = {
   formatCurrency,
   createElement,
+  createTreeElement,
   appendProduct,
 };
